0d76d7d1e244fe37d835b0ae5e8a38614f56e4f2
damianmdr <damianmr@live.com>
2026-01-02 21:17:24 -0300

    working rotation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_RADIUS
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -8,10 +9,24 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while 1 == 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return          
+        
         log_state()
         screen.fill("black")
-        pygame.display.flip()
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip() 
+        dt = clock.tick(60) / 1000
+  
+
+
+
 
 if __name__ == "__main__":
     main()
